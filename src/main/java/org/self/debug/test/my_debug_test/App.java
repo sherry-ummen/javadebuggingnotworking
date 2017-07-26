30d7e2253f6f2ef7cf2dcf399e88dec4bad6f958
Sherry Ummen <sherry.ummen@m-brain.com>
2017-07-26 11:52:22 +0300

    why I am not able to remote debug :(

---
+++ src/main/java/org/self/debug/test/my_debug_test/App.java
@@ -0,0 +1,19 @@
+package org.self.debug.test.my_debug_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int test = 20;
+        System.out.println( "Hello World! " + test );
+        
+    }
+    
+    public static void test() {
+    	System.out.print("Hello");
+    }
+}
